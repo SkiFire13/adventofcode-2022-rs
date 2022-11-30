f6f1e387c3e30c98761a6d90f53acf9d782f078d
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-11-30 20:43:20 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions for the Advent of Code 2022.
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+// Makes using `Input` type aliases awkward
+#![allow(clippy::ptr_arg)]
+
+aoc_helper::main!(2022 =>
+    // day1,
+    // day2,
+    // day3,
+    // day4,
+    // day5,
+    // day6,
+    // day7,
+    // day8,
+    // day9,
+    // day10,
+    // day11,
+    // day12,
+    // day13,
+    // day14,
+    // day15,
+    // day16,
+    // day17,
+    // day18,
+    // day19,
+    // day20,
+    // day21,
+    // day22,
+    // day23,
+    // day24,
+    // day25,
+);
