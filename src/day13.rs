b3cc14498269f447147a668fc69df7534e92eec4
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-12-13 15:30:40 +0100

    Improve day 13 solution

--- src/day13.rs
+++ src/day13.rs
@@ -1,88 +1,68 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<[ListElem; 2]>;
+type Input = Vec<Packet>;
 
-pub enum ListElem {
+pub enum Packet {
     Int(u8),
-    List(Vec<ListElem>),
+    Seq(Vec<Packet>),
 }
 
-fn parse_list_elem(s: &str) -> ListElem {
-    fn parse_inner(s: &mut &[u8]) -> ListElem {
-        match eat_copy(s) {
-            b'[' => {
-                let mut l = Vec::new();
-                if s[0] != b']' {
-                    l.push(parse_inner(s));
-                }
-                while eat_copy(s) == b',' {
-                    l.push(parse_inner(s));
-                }
-                ListElem::List(l)
+fn parse_list_elem(input: &str) -> Packet {
+    fn parse_inner(input: &mut &[u8]) -> Packet {
+        let mut seq = Vec::new();
+        let mut acc = None;
+        loop {
+            match eat_copy(input) {
+                b'[' => seq.push(parse_inner(input)),
+                b @ b'0'..=b'9' => acc = Some(acc.unwrap_or(0) * 10 + b - b'0'),
+                b',' => seq.extend(acc.take().map(Packet::Int)),
+                b']' => break,
+                _ => panic!("Invalid input"),
             }
-            b => ListElem::Int(
-                eat_while(s, u8::is_ascii_digit)
-                    .iter()
-                    .fold(b - b'0', |acc, b| acc * 10 + b - b'0'),
-            ),
         }
+        seq.extend(acc.take().map(Packet::Int));
+        Packet::Seq(seq)
     }
-    parse_inner(&mut s.as_bytes())
+    parse_inner(&mut input[1..].as_bytes())
 }
 
 pub fn input_generator(input: &str) -> Input {
     input
-        .split("\n\n")
-        .map(|line| {
-            let (l1, l2) = line.split_once('\n').expect("Invalid input");
-            [parse_list_elem(l1), parse_list_elem(l2)]
-        })
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(parse_list_elem)
         .collect()
 }
 
-impl PartialEq for ListElem {
-    fn eq(&self, other: &Self) -> bool {
-        self.cmp(other).is_eq()
-    }
-}
-
-impl Eq for ListElem {}
-impl PartialOrd for ListElem {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for ListElem {
-    fn cmp(&self, other: &ListElem) -> Ordering {
-        use std::slice::from_ref as slice_from_ref;
-        let (l1, l2) = (self, other);
-        let (l1, l2) = match (l1, l2) {
-            (ListElem::List(l1), ListElem::List(l2)) => (&l1[..], &l2[..]),
-            (ListElem::List(l1), ListElem::Int(_)) => (&l1[..], slice_from_ref(l2)),
-            (ListElem::Int(_), ListElem::List(l2)) => (slice_from_ref(l1), &l2[..]),
-            (ListElem::Int(i1), ListElem::Int(i2)) => return i1.cmp(&i2),
-        };
-        l1.cmp(l2)
-    }
+fn cmp(p1: &Packet, p2: &Packet) -> Ordering {
+    use std::slice::from_ref as slice_from_ref;
+    let (s1, s2) = match (p1, p2) {
+        (Packet::Seq(l1), Packet::Seq(l2)) => (&l1[..], &l2[..]),
+        (Packet::Seq(l1), Packet::Int(_)) => (&l1[..], slice_from_ref(p2)),
+        (Packet::Int(_), Packet::Seq(l2)) => (slice_from_ref(p1), &l2[..]),
+        (Packet::Int(i1), Packet::Int(i2)) => return u8::cmp(&i1, &i2),
+    };
+    iter::zip(s1, s2)
+        .map(|(p1, p2)| cmp(p1, p2))
+        .find(|ord| ord.is_ne())
+        .unwrap_or(usize::cmp(&s1.len(), &s2.len()))
 }
 
 pub fn part1(input: &Input) -> usize {
     input
         .iter()
+        .tuples()
         .enumerate()
-        .filter(|&(_, [l1, l2])| l1 <= l2)
+        .filter(|&(_, (p1, p2))| cmp(p1, p2).is_lt())
         .map(|(idx, _)| idx + 1)
         .sum()
 }
 
 pub fn part2(input: &Input) -> usize {
-    let mut packets = input.iter().flatten().collect::<Vec<_>>();
-    let p1 = ListElem::Int(2);
-    let p2 = ListElem::Int(6);
-    packets.extend([&p1, &p2]);
-    packets.sort_unstable();
-    let i1 = packets.binary_search(&&p1).unwrap();
-    let i2 = packets.binary_search(&&p2).unwrap();
-    (i1 + 1) * (i2 + 1)
+    let (l, b) = input.iter().fold((1, input.len() + 2), |(l, u), p| {
+        let lower_than_2 = cmp(p, &Packet::Int(2)).is_lt();
+        let bigger_than_6 = !lower_than_2 && cmp(p, &Packet::Int(6)).is_gt();
+        (l + lower_than_2 as usize, u - bigger_than_6 as usize)
+    });
+    l * b
 }
