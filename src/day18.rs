04ca1e3e869c9d08c0d3d1674c11e120e91ca403
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-12-18 11:58:34 +0100

    Make day 18 solution more general

--- src/day18.rs
+++ src/day18.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<(i16, i16, i16)>;
+type Input = Vec<(usize, usize, usize)>;
 
 pub fn input_generator(input: &str) -> Input {
     input
@@ -15,74 +15,55 @@ pub fn input_generator(input: &str) -> Input {
         .collect()
 }
 
-fn solve(input: &Input, check_block: impl Fn(i16, i16, i16) -> bool) -> usize {
-    input
-        .iter()
-        .flat_map(|&(x, y, z)| {
-            [
-                (x - 1, y, z),
-                (x + 1, y, z),
-                (x, y - 1, z),
-                (x, y + 1, z),
-                (x, y, z - 1),
-                (x, y, z + 1),
-            ]
-        })
-        .filter(|&(x, y, z)| check_block(x, y, z))
-        .count()
+fn solve(input: &Input, count_sides: impl Fn(usize, usize, usize) -> usize) -> usize {
+    input.iter().map(|&(x, y, z)| count_sides(x, y, z)).sum()
 }
 
 fn lens(input: &Input) -> (usize, usize, usize) {
-    let (mut lenx, mut leny, mut lenz) = (0, 0, 0);
-    for &(x, y, z) in input {
-        lenx = max(lenx, x as usize + 1);
-        leny = max(leny, y as usize + 1);
-        lenz = max(lenz, z as usize + 1);
-    }
-    (lenx, leny, lenz)
+    input
+        .iter()
+        .fold((0, 0, 0), |(lenx, leny, lenz), &(x, y, z)| {
+            (max(lenx, x + 1), max(leny, y + 1), max(lenz, z + 1))
+        })
 }
 
 pub fn part1(input: &Input) -> usize {
     let (lenx, leny, lenz) = lens(input);
 
-    let mut set = vec![false; lenx * leny * lenz];
+    let mut set = Grid3D::with_dimensions(lenx + 2, leny + 2, lenz + 2).into_set();
     for &(x, y, z) in input {
-        let (x, y, z) = (x as usize, y as usize, z as usize);
-        set[x + lenx * (y + leny * z)] = true;
+        set.insert((x + 1, y + 1, z + 1));
     }
+
     solve(input, |x, y, z| {
-        let xok = 0 <= x && x < lenx as i16;
-        let yok = 0 <= y && y < leny as i16;
-        let zok = 0 <= z && z < lenz as i16;
-        let (x, y, z) = (x as usize, y as usize, z as usize);
-        !(xok && yok && zok && set[x + lenx * (y + leny * z)])
+        set.plus_neighbours((x + 1, y + 1, z + 1))
+            .filter(|&p| !set.contains(p))
+            .count()
     })
 }
 
 pub fn part2(input: &Input) -> usize {
     let (lenx, leny, lenz) = lens(input);
-    let init = |_, _| (i16::MAX, 0);
-    let mut minmaxx = Grid::with_dimensions_init(leny + 1, lenz + 1, init);
-    let mut minmaxy = Grid::with_dimensions_init(lenx + 1, lenz + 1, init);
-    let mut minmaxz = Grid::with_dimensions_init(lenx + 1, leny + 1, init);
 
+    let mut set = Grid3D::with_dimensions(lenx + 2, leny + 2, lenz + 2).into_set();
     for &(x, y, z) in input {
-        let merge = |minmax: &mut Grid<_>, (p0, p1), c| {
-            let (minc, maxc) = &mut minmax[(p0 as usize, p1 as usize)];
-            *minc = min(*minc, c);
-            *maxc = max(*maxc, c);
-        };
-        merge(&mut minmaxx, (y, z), x);
-        merge(&mut minmaxy, (x, z), y);
-        merge(&mut minmaxz, (x, y), z);
+        set.insert((x + 1, y + 1, z + 1));
     }
-    let check_inside = |minmax: &Grid<_>, (p0, p1), c| {
-        let Some(&(min, max)) = minmax.iget((p0 as isize, p1 as isize)) else { return false };
-        min <= c && c <= max
-    };
+
+    let mut wrapper = Grid3D::with_dimensions(lenx + 2, leny + 2, lenz + 2).into_set();
+    let mut stack = vec![(0, 0, 0)];
+    while let Some((x, y, z)) = stack.pop() {
+        for (x, y, z) in set.plus_neighbours((x, y, z)) {
+            if !set.contains((x, y, z)) && wrapper.insert((x, y, z)) {
+                stack.push((x, y, z))
+            }
+        }
+    }
+
     solve(input, |x, y, z| {
-        !check_inside(&minmaxx, (y, z), x)
-            || !check_inside(&minmaxy, (x, z), y)
-            || !check_inside(&minmaxz, (x, y), z)
+        wrapper
+            .plus_neighbours((x + 1, y + 1, z + 1))
+            .filter(|&p| wrapper.contains(p))
+            .count()
     })
 }
