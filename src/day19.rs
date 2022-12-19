e4a5b41ef458e9e47f78621d84e0d7aa88d731bf
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-12-19 15:47:26 +0100

    Clean up day 19 solution

--- src/day19.rs
+++ src/day19.rs
@@ -1,300 +1,192 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<(usize, usize, usize, (usize, usize), (usize, usize))>;
+type Input = Vec<BluePrint>;
 
 pub fn input_generator(input: &str) -> Input {
-    //     "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
-    // Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."
     input
         .lines()
         .map(|line| {
-            let (_, rest) = line.split_once("Blueprint ").unwrap();
-            let (blueprint, rest) = rest.split_once(": Each ore robot costs ").unwrap();
-            let (ore_cost, rest) = rest.split_once(" ore. Each clay robot costs ").unwrap();
-            let (clay_cost, rest) = rest.split_once(" ore. Each obsidian robot costs ").unwrap();
-            let (ob_ore_cost, rest) = rest.split_once(" ore and ").unwrap();
-            let (ob_clay_cost, rest) = rest.split_once(" clay. Each geode robot costs ").unwrap();
-            let (geode_ore_cost, rest) = rest.split_once(" ore and ").unwrap();
-            let (geode_ob_cost, _) = rest.split_once(" obsidian.").unwrap();
-            let blueprint = blueprint.parse().unwrap();
-            let ore_cost = ore_cost.parse().unwrap();
-            let clay_cost = clay_cost.parse().unwrap();
-            let ob_ore_cost = ob_ore_cost.parse().unwrap();
-            let ob_clay_cost = ob_clay_cost.parse().unwrap();
-            let geode_ore_cost = geode_ore_cost.parse().unwrap();
-            let geode_ob_cost = geode_ob_cost.parse().unwrap();
-            (
-                blueprint,
-                ore_cost,
-                clay_cost,
-                (ob_ore_cost, ob_clay_cost),
-                (geode_ore_cost, geode_ob_cost),
-            )
+            let (_, rest) = line.split_once("Blueprint ").expect("Invalid input");
+            let (id, rest) = rest.split_once(":").expect("Invalid input");
+            let (_, rest) = rest.split_once(" costs ").expect("Invalid input");
+            let (ore_ore_cost, rest) = rest.split_once(" ore").expect("Invalid input");
+            let (_, rest) = rest.split_once(" costs ").expect("Invalid input");
+            let (clay_ore_cost, rest) = rest.split_once(" ore").expect("Invalid input");
+            let (_, rest) = rest.split_once(" costs ").expect("Invalid input");
+            let (obs_ore_cost, rest) = rest.split_once(" ore and ").expect("Invalid input");
+            let (obs_clay_cost, rest) = rest.split_once(" clay").expect("Invalid input");
+            let (_, rest) = rest.split_once(" costs ").expect("Invalid input");
+            let (geode_ore_cost, rest) = rest.split_once(" ore and ").expect("Invalid input");
+            let (geode_obs_cost, _) = rest.split_once(" obsidian.").expect("Invalid input");
+            let id = id.parse().expect("Invalid input");
+            let ore_ore_cost = ore_ore_cost.parse().expect("Invalid input");
+            let clay_ore_cost = clay_ore_cost.parse().expect("Invalid input");
+            let obs_ore_cost = obs_ore_cost.parse().expect("Invalid input");
+            let obs_clay_cost = obs_clay_cost.parse().expect("Invalid input");
+            let geode_ore_cost = geode_ore_cost.parse().expect("Invalid input");
+            let geode_obs_cost = geode_obs_cost.parse().expect("Invalid input");
+            BluePrint {
+                id,
+                ore_ore_cost,
+                clay_ore_cost,
+                obs_ore_cost,
+                obs_clay_cost,
+                geode_ore_cost,
+                geode_obs_cost,
+            }
         })
         .collect()
 }
 
+pub struct BluePrint {
+    id: usize,
+    ore_ore_cost: u16,
+    clay_ore_cost: u16,
+    obs_ore_cost: u16,
+    obs_clay_cost: u16,
+    geode_ore_cost: u16,
+    geode_obs_cost: u16,
+}
+
+fn solve(blueprint: &BluePrint, time: u16) -> usize {
+    let &BluePrint {
+        id: _,
+        ore_ore_cost,
+        clay_ore_cost,
+        obs_ore_cost,
+        obs_clay_cost,
+        geode_ore_cost,
+        geode_obs_cost,
+    } = blueprint;
+
+    let max_ore_cost = max(clay_ore_cost, max(obs_ore_cost, geode_obs_cost));
+    let max_clay_cost = obs_clay_cost;
+    let max_obs_cost = geode_obs_cost;
+
+    #[derive(Clone, Copy, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    struct Entry {
+        upper_bound: u16,
+        geode: u16,
+        time: u16,
+        obs_miners: u16,
+        clay_miners: u16,
+        ore_miners: u16,
+        ore: u16,
+        clay: u16,
+        obs: u16,
+    }
+
+    let initial = Entry { upper_bound: 1, time, ore_miners: 1, ..Default::default() };
+    let mut stack = Vec::from([initial]);
+    let mut seen = HashSet::new();
+
+    let mut best = 0;
+
+    while let Some(entry) = stack.pop() {
+        if entry.upper_bound as usize <= best || !seen.insert(entry) {
+            continue;
+        }
+
+        if entry.time == 0
+            || (entry.ore >= geode_ore_cost
+                && entry.obs >= geode_obs_cost
+                && entry.ore_miners >= geode_ore_cost
+                && entry.obs_miners >= geode_obs_cost)
+        {
+            best = max(best, entry.upper_bound as usize);
+            continue;
+        }
+
+        let mut new_entry = entry;
+        new_entry.ore += entry.ore_miners;
+        new_entry.clay += entry.clay_miners;
+        new_entry.obs += entry.obs_miners;
+        (entry.ore_miners == max_ore_cost).then(|| new_entry.ore = max_ore_cost);
+        (entry.clay_miners == max_clay_cost).then(|| new_entry.clay = max_clay_cost);
+        (entry.obs_miners == max_obs_cost).then(|| new_entry.obs = max_obs_cost);
+
+        new_entry.time -= 1;
+
+        let add_upper_bound = |miners, limiting_factors: &[_]| {
+            let time = new_entry.time;
+            let &miners_upper_bound = limiting_factors.iter().chain([&time]).min().unwrap();
+            miners * time
+                + miners_upper_bound * miners_upper_bound.saturating_sub(1) / 2
+                + miners_upper_bound * (time - miners_upper_bound)
+        };
+
+        let ore_upper_bound = new_entry.ore + add_upper_bound(new_entry.ore_miners, &[]);
+        let clay_upper_bound = new_entry.clay
+            + add_upper_bound(new_entry.clay_miners, &[ore_upper_bound / clay_ore_cost]);
+        let obs_limiting = &[
+            ore_upper_bound / obs_ore_cost,
+            clay_upper_bound / obs_clay_cost,
+        ];
+        let obs_upper_bound = new_entry.obs + add_upper_bound(new_entry.obs_miners, obs_limiting);
+        let geode_limiting = &[
+            ore_upper_bound / geode_ore_cost,
+            obs_upper_bound / geode_obs_cost,
+        ];
+        new_entry.upper_bound = new_entry.geode + add_upper_bound(0, geode_limiting);
+
+        if new_entry.time == 0
+            || entry.ore < max_ore_cost
+            || entry.clay < max_clay_cost
+            || entry.obs < max_obs_cost
+        {
+            stack.push(new_entry)
+        };
+
+        if new_entry.time > 0 {
+            if entry.ore >= ore_ore_cost && entry.ore_miners < max_ore_cost {
+                let mut new_entry = new_entry;
+                new_entry.ore_miners += 1;
+                new_entry.ore -= ore_ore_cost;
+                stack.push(new_entry);
+            }
+
+            if entry.ore >= clay_ore_cost && entry.clay_miners < max_clay_cost {
+                let mut new_entry = new_entry;
+                new_entry.clay_miners += 1;
+                new_entry.ore -= clay_ore_cost;
+                stack.push(new_entry);
+            }
+
+            if entry.ore >= obs_ore_cost
+                && entry.clay >= obs_clay_cost
+                && entry.obs_miners < max_obs_cost
+            {
+                let mut new_entry = new_entry;
+                new_entry.obs_miners += 1;
+                new_entry.ore -= obs_ore_cost;
+                new_entry.clay -= obs_clay_cost;
+                stack.push(new_entry);
+            }
+
+            if entry.ore >= geode_ore_cost && entry.obs >= geode_obs_cost {
+                let mut new_entry = new_entry;
+                new_entry.upper_bound += new_entry.time;
+                new_entry.geode += new_entry.time;
+                new_entry.ore -= geode_ore_cost;
+                new_entry.obs -= geode_obs_cost;
+                stack.push(new_entry);
+            }
+        }
+    }
+
+    best
+}
+
 pub fn part1(input: &Input) -> usize {
-    return 0;
     input
         .par_iter()
-        .map(
-            |&(
-                blueprint,
-                ore_ore_cost,
-                clay_ore_cost,
-                (ob_ore_cost, ob_clay_cost),
-                (geode_ore_cost, geode_ob_cost),
-            )| {
-                let mut queue = BinaryHeap::from([(22, 0, 24, 0, 0, 0, 1, 0, 0, 0)]);
-                let mut seen = HashSet::new();
-
-                while let Some((
-                    _,
-                    geode,
-                    time,
-                    geode_miners,
-                    ob_miners,
-                    clay_miners,
-                    ore_miners,
-                    ore,
-                    clay,
-                    obs,
-                )) = queue.pop()
-                {
-                    if time == 0 {
-                        return dbg!(dbg!(geode) * blueprint);
-                    }
-
-                    if !seen.insert((
-                        geode,
-                        time,
-                        geode_miners,
-                        ob_miners,
-                        clay_miners,
-                        ore_miners,
-                        ore,
-                        clay,
-                        obs,
-                    )) {
-                        continue;
-                    }
-
-                    let new_ore = ore + ore_miners;
-                    let new_clay = clay + clay_miners;
-                    let new_obs = obs + ob_miners;
-                    let new_geode = geode + geode_miners;
-
-                    queue.push((
-                        new_geode + (geode_miners + time - 2) * (time - 2),
-                        new_geode,
-                        time - 1,
-                        geode_miners,
-                        ob_miners,
-                        clay_miners,
-                        ore_miners,
-                        new_ore,
-                        new_clay,
-                        new_obs,
-                    ));
-
-                    if time > 1 {
-                        if ore >= ore_ore_cost {
-                            queue.push((
-                                new_geode + (geode_miners + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners,
-                                ob_miners,
-                                clay_miners,
-                                ore_miners + 1,
-                                new_ore - ore_ore_cost,
-                                new_clay,
-                                new_obs,
-                            ));
-                        }
-
-                        if ore >= clay_ore_cost {
-                            queue.push((
-                                new_geode + (geode_miners + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners,
-                                ob_miners,
-                                clay_miners + 1,
-                                ore_miners,
-                                new_ore - clay_ore_cost,
-                                new_clay,
-                                new_obs,
-                            ));
-                        }
-
-                        if ore >= ob_ore_cost && clay >= ob_clay_cost {
-                            queue.push((
-                                new_geode + (geode_miners + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners,
-                                ob_miners + 1,
-                                clay_miners,
-                                ore_miners,
-                                new_ore - ob_ore_cost,
-                                new_clay - ob_clay_cost,
-                                new_obs,
-                            ));
-                        }
-
-                        if ore >= geode_ore_cost && obs >= geode_ob_cost {
-                            queue.push((
-                                new_geode + 1 + (geode_miners + 1 + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners + 1,
-                                ob_miners,
-                                clay_miners,
-                                ore_miners,
-                                new_ore - geode_ore_cost,
-                                new_clay,
-                                new_obs - geode_ob_cost,
-                            ));
-                        }
-                    }
-                }
-
-                unreachable!()
-            },
-        )
+        .map(|blueprint| blueprint.id * solve(blueprint, 24))
         .sum()
 }
 
 pub fn part2(input: &Input) -> usize {
     input[..3]
         .par_iter()
-        .map(
-            |&(
-                _,
-                ore_ore_cost,
-                clay_ore_cost,
-                (ob_ore_cost, ob_clay_cost),
-                (geode_ore_cost, geode_ob_cost),
-            )| {
-                let mut queue = BinaryHeap::from([(30, 0, 32, 0, 0, 0, 1, 0, 0, 0)]);
-                let mut seen = HashSet::new();
-
-                while let Some((
-                    _,
-                    geode,
-                    time,
-                    geode_miners,
-                    ob_miners,
-                    clay_miners,
-                    ore_miners,
-                    ore,
-                    clay,
-                    obs,
-                )) = queue.pop()
-                {
-                    if time == 0 {
-                        return dbg!(geode);
-                    }
-
-                    if !seen.insert((
-                        geode,
-                        time,
-                        geode_miners,
-                        ob_miners,
-                        clay_miners,
-                        ore_miners,
-                        ore,
-                        clay,
-                        obs,
-                    )) {
-                        continue;
-                    }
-
-                    let new_ore = ore + ore_miners;
-                    let new_clay = clay + clay_miners;
-                    let new_obs = obs + ob_miners;
-                    let new_geode = geode + geode_miners;
-
-                    queue.push((
-                        new_geode + (geode_miners + time - 2) * (time - 2),
-                        new_geode,
-                        time - 1,
-                        geode_miners,
-                        ob_miners,
-                        clay_miners,
-                        ore_miners,
-                        new_ore,
-                        new_clay,
-                        new_obs,
-                    ));
-
-                    if time > 1 {
-                        if ore >= ore_ore_cost {
-                            queue.push((
-                                new_geode + (geode_miners + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners,
-                                ob_miners,
-                                clay_miners,
-                                ore_miners + 1,
-                                new_ore - ore_ore_cost,
-                                new_clay,
-                                new_obs,
-                            ));
-                        }
-
-                        if ore >= clay_ore_cost {
-                            queue.push((
-                                new_geode + (geode_miners + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners,
-                                ob_miners,
-                                clay_miners + 1,
-                                ore_miners,
-                                new_ore - clay_ore_cost,
-                                new_clay,
-                                new_obs,
-                            ));
-                        }
-
-                        if ore >= ob_ore_cost && clay >= ob_clay_cost {
-                            queue.push((
-                                new_geode + (geode_miners + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners,
-                                ob_miners + 1,
-                                clay_miners,
-                                ore_miners,
-                                new_ore - ob_ore_cost,
-                                new_clay - ob_clay_cost,
-                                new_obs,
-                            ));
-                        }
-
-                        if ore >= geode_ore_cost && obs >= geode_ob_cost {
-                            queue.push((
-                                new_geode + 1 + (geode_miners + 1 + time - 2) * (time - 2),
-                                new_geode,
-                                time - 1,
-                                geode_miners + 1,
-                                ob_miners,
-                                clay_miners,
-                                ore_miners,
-                                new_ore - geode_ore_cost,
-                                new_clay,
-                                new_obs - geode_ob_cost,
-                            ));
-                        }
-                    }
-                }
-
-                unreachable!()
-            },
-        )
+        .map(|blueprint| solve(blueprint, 32))
         .product()
 }
