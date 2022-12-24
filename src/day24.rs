3b6a2e0a87f18588e6b1da20a58a6417a7ae77ea
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-12-24 10:51:04 +0100

    Improve day 24 solution

--- src/day24.rs
+++ src/day24.rs
@@ -1,90 +1,73 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = (
-    (isize, isize),
-    HashMap<(isize, isize), Vec<(isize, isize)>>,
-    (isize, isize),
-);
+type Input = Grid<(i8, i8)>;
 
 pub fn input_generator(input: &str) -> Input {
-    let (start_line, rest) = input.split_once('\n').unwrap();
-    let startx = start_line.chars().position(|c| c == '.').unwrap() as isize - 1;
-    let (rest, end_line) = rest.rsplit_once('\n').unwrap();
-    let endx = end_line.chars().position(|c| c == '.').unwrap() as isize - 1;
-    let endy = rest.lines().count() as isize;
-    let blizzards = rest
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.chars().enumerate().filter_map(move |(x, c)| {
-                let (x, y) = (x as isize, y as isize);
-                Some(match c {
-                    '.' | '#' => return None,
-                    '>' => ((x - 1, y), vec![(1, 0)]),
-                    '<' => ((x - 1, y), vec![(-1, 0)]),
-                    'v' => ((x - 1, y), vec![(0, 1)]),
-                    '^' => ((x - 1, y), vec![(0, -1)]),
-                    _ => panic!(),
-                })
-            })
-        })
-        .collect();
-    ((startx, -1), blizzards, (endx, endy))
-}
+    let lenx = input.find('\n').expect("Invalid input") as isize - 2;
+    let leny = input.lines().count() as isize - 2;
 
-fn solve(input: &Input, end_state: usize) -> usize {
-    let (start, mut blizzards, end) = input.clone();
-
-    let lenx = end.0 + 1;
-    let leny = end.1;
-
-    let mut iter = 0;
-    let mut curr_stack = vec![(start, 0)];
-    let mut next_stack = Vec::new();
-    let mut seen = HashSet::new();
-
-    loop {
-        seen.clear();
-
-        blizzards = std::mem::take(&mut blizzards)
-            .into_iter()
-            .flat_map(|((x, y), ds)| {
-                ds.into_iter().map(move |(dx, dy)| {
-                    (((x + dx + lenx) % lenx, (y + dy + leny) % leny), (dx, dy))
-                })
-            })
-            .into_group_map();
-
-        for (pos @ (x, y), mut state) in curr_stack.drain(..) {
-            if pos == end && state == end_state {
-                return iter;
-            }
-            if pos == end && state % 2 == 0 {
-                state += 1;
-            }
-            if pos == start && state % 2 == 1 {
-                state += 1;
-            }
-
-            for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)] {
-                let new_pos @ (x, y) = (x + dx, y + dy);
-                if new_pos == start
-                    || new_pos == end
-                    || (0 <= x
-                        && x < lenx
-                        && 0 <= y
-                        && y < leny
-                        && !blizzards.contains_key(&new_pos))
-                        && seen.insert((new_pos, state))
-                {
-                    next_stack.push((new_pos, state));
-                }
+    let mut blizzards = Grid::with_dimensions(lenx as usize, leny as usize);
+    for (y, line) in input.lines().dropping(1).dropping_back(1).enumerate() {
+        for (x, c) in line.chars().dropping(1).dropping_back(1).enumerate() {
+            let (x, y) = (x as isize, y as isize);
+            match c {
+                '.' => {}
+                '>' => blizzards[(x, y)] = (1, 0),
+                '<' => blizzards[(x, y)] = (-1, 0),
+                'v' => blizzards[(x, y)] = (0, 1),
+                '^' => blizzards[(x, y)] = (0, -1),
+                _ => panic!(),
             }
         }
-
-        std::mem::swap(&mut curr_stack, &mut next_stack);
-        iter += 1;
     }
+
+    blizzards
+}
+
+fn solve(blizzards: &Input, max_iter: usize) -> usize {
+    let lenx = blizzards.w() as isize;
+    let leny = blizzards.h() as isize;
+    let (mut start, mut end) = ((0, -1), (lenx - 1, leny));
+
+    let mut iter = 0;
+    let mut queue = BinaryHeap::from([(Reverse(0), Reverse(0), start)]);
+    let mut seen = FxHashSet::default();
+
+    while let Some((_, Reverse(steps), pos @ (x, y))) = queue.pop() {
+        if pos == end {
+            if iter == max_iter {
+                return steps as usize;
+            }
+            (start, end) = (end, start);
+            queue.clear();
+            seen.clear();
+            iter += 1;
+        }
+
+        'next: for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)] {
+            let (x, y) = (x + dx, y + dy);
+            if (x, y) != start && (x, y) != end {
+                if !(0 <= x && x < lenx && 0 <= y && y < leny) {
+                    continue 'next;
+                }
+                for (bdx, bdy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                    let bx = (x - bdx * (steps + 1)).rem_euclid(lenx) as usize;
+                    let by = (y - bdy * (steps + 1)).rem_euclid(leny) as usize;
+                    if blizzards[(bx, by)] == (bdx as i8, bdy as i8) {
+                        continue 'next;
+                    }
+                }
+                if !seen.insert((x, y, steps)) {
+                    continue 'next;
+                }
+            }
+            let (endx, endy) = end;
+            let lower_bound = steps + 1 + isize::abs(endx - x) + isize::abs(endy - y);
+            queue.push((Reverse(lower_bound), Reverse(steps + 1), (x, y)));
+        }
+    }
+
+    panic!("Invalid input")
 }
 
 pub fn part1(input: &Input) -> usize {
